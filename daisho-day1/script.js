2c135835ff68aaf9ad39b05d4bca66ce85d5d903
sakucha1717-stack <sa.ku.cha.1717@gmail.com>
2025-10-16 15:07:29 +0900

    20251016v07

--- daisho-day1/script.js
+++ daisho-day1/script.js
@@ -3,11 +3,11 @@ const message = document.getElementByld("message");
 
 helloBtn.addEventListener("click",() => {
     const hour = new Date().getHours();
-    let greeting = "こんにちは！";
-    if(hour < 12){
-        greeting = "おはようございます！";
-    }else if (hour >= 18){
-        greeting = "こんばんは！";
+    let greeting ="こんにちは！";
+    if(hour<12){
+        greeting ="おはようございます！";
+    }else if (hour>=18){
+        greeting ="こんばんは！";
     }
-    message.textContent = '${greeting} GitHub Pages の公開に成功しました！';
+    message.textContent ='${greeting} GitHub Pages の公開に成功しました！';
 })
